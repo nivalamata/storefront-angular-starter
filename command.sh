10f91cfd8dcfcc1e771d2f16e72bc80e27ff3a02
qude <gmlwilo@gmail.com>
2023-12-09 17:06:34 +0300

    Bump version to v0.3.2

--- command.sh
+++ command.sh
@@ -1,3 +1,4 @@
-git tag -a v0.3.1 -m "v0.3.1"
+git tag -a v0.3.2 -m "v0.3.2"
 git push origin master --follow-tags
 git push origin develop --follow-tags
+
--- install-storefront.sh
+++ install-storefront.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd "${0%/*}"
 echo Fetching storefront $1...
-curl https://vendure-storefront-artifacts.s3.eu-central-1.amazonaws.com/$1/vendure-storefront-build-$1.zip -L -o storefront.zip
+#https://nivalamata.s3.eu-central-1.amazonaws.com/v0.3.2/vendure-storefront-build-v0.3.2.zip
+curl https://nivalamata.s3.eu-central-1.amazonaws.com/$1/vendure-storefront-build-$1.zip -L -o storefront.zip
 unzip -o storefront.zip
